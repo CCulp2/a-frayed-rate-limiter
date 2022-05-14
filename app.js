33f3dcd9defb93b4aa20b70532bad2adb398d6e5
CCulp2 <80595975+CCulp2@users.noreply.github.com>
2022-05-14 00:48:29 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const REQUESTS_PER_FIVE_SECONDS = 25;
+const slowDown = require('express-slow-down');
+const app = express();
+const port = 6969;
+
+const speedLimiter = slowDown({
+    windowMs: 5000,
+    delayAfter: REQUESTS_PER_FIVE_SECONDS,
+    delayMs: 5000
+})
+
+app.use(speedLimiter);
+
+app.get('/:urlOfRequest', (req, res) => {
+    // FETCH here
+    // get URL from req.params.urlOfRequest
+    // Build call from req.body, req.headers
+    // RES => build same way.
+});
+
+app.post('/:urlOfRequest', (req, res) => {
+    // FETCH here
+    // get URL from req.params.urlOfRequest
+    // Build call from req.body, req.headers
+    // RES => build same way
+
+})
